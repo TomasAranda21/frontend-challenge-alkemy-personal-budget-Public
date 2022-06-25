cc191a0526ac702a8d106348ea93bdd73703183b
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-25 19:07:02 -0300

    prepare frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="font-bold text-5xl">
+      <h1>Hola</h1>
+    </div>
+  )
+}
+
+export default App
