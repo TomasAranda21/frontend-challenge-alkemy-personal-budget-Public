eccdf57f7df6a133a3eea480033b3cce3f840766
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-25 19:27:53 -0300

    use context to get user

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import { createContext } from 'react'
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({children}) => {
+
+
+
+  return (
+    <AuthContext.Provider
+    value={{
+
+    }}
+    >
+
+    {children}
+
+    </AuthContext.Provider>
+  )
+
+
+
+}
+
+
+export default AuthContext
