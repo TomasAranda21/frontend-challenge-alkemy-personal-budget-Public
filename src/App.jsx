b2524e68b7d996ef18e3555ba52a6ce2034f9284
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-25 21:48:57 -0300

    functionality for login, registration and authentication through the axios request

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ButtonForm from './components/Buttons/ButtonForm'
 import { AuthProvider } from './context/AuthContext'
+
 import AuthLayout from './layout/AuthLayout'
-import Login from './pages/login'
-import Register from './pages/register'
+import PrivateLayout from './layout/PrivateLayout'
+
+
+import Login from './pages/LoginUser'
+import Register from './pages/RegisterUser'
+import Home from './pages/HomeUser'
 
 
 function App() {
 
   return (
-
+    
     <BrowserRouter>
       <AuthProvider>
 
           <Routes>
 
+
             <Route path="/" element={<AuthLayout/>}>
 
-                <Route index element={ <Login />}/>
-
-                <Route path="sign-in" element={ <Register />} />
-
+                  <Route index element={<Login />} /> 
+             
+            
+                  <Route path="sign-in" element={ <Register />} />
+          
             </Route>
 
+
+
+            <Route path="/home" element={<PrivateLayout/>}>
+
+                  <Route index element={<Home />} /> 
+             
+            
+                  {/* <Route path="sign-in" element={ <Register />} /> */}
+          
+            </Route>
 
 
 
@@ -31,6 +49,7 @@ function App() {
 
       </AuthProvider>
     </BrowserRouter>
+    
   )
 }
 
--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
+
+    return (
+
+        <main className='container mx-auto md:grid md:grid-cols-2 mt-10 p-10 gap-10'>
+
+            <Outlet/> 
+    
+        </main>
+    )
 }
+
 
 export default AuthLayout
